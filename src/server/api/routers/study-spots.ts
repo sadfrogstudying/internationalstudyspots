629f2fd28942460222568567331e204423a7a0a1
Charlie Zhao <charles.zhao5461@gmail.com>
2023-11-15 15:27:34 +1100

    remove optional from endpoint input so typechecking works again

--- src/server/api/routers/study-spots.ts
+++ src/server/api/routers/study-spots.ts
@@ -1,4 +1,4 @@
-import { object, number, string } from "zod";
+import { object, number, string, boolean } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { createSpotSchemaServer } from "@/schemas";
@@ -9,12 +9,26 @@ import {
 
 export const studySpotRouter = createTRPCRouter({
   getAll: publicProcedure
-    .input(object({ cursor: number().optional() }).optional())
+    .input(
+      object({
+        cursor: number().optional(),
+        where: object({
+          powerOutlets: boolean().optional(),
+          wifi: boolean().optional(),
+        }),
+      }),
+    )
     .query(async ({ ctx, input }) => {
-      const { cursor } = input ?? {};
+      const { cursor, where } = input;
 
       const spots = await getManyHandler(ctx.db, {
         ...(cursor && { skip: 1, cursor: { id: cursor } }),
+        ...(where && {
+          where: {
+            powerOutlets: where.powerOutlets,
+            wifi: where.wifi,
+          },
+        }),
       });
 
       return spots;

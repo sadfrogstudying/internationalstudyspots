46a82788f516fe64afd2dcd9de0c821fb6ecc026
Charlie Zhao <charles.zhao5461@gmail.com>
2023-12-22 15:34:09 +1100

    Add asChild to Link

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -3,21 +3,28 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import NextLink from "next/link";
 
-const Link = React.forwardRef<
-  HTMLAnchorElement,
-  React.ComponentProps<typeof NextLink>
->(({ className, ...props }, ref) => {
-  return (
-    <NextLink
-      className={cn(
-        "rounded-sm outline-2 outline-offset-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:outline active:outline-purple-600",
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+import { Slot } from "@radix-ui/react-slot";
+
+type LinkProps = React.ComponentProps<typeof NextLink> & {
+  asChild?: boolean;
+};
+
+const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  ({ className, asChild, ...props }, ref) => {
+    const Comp = asChild ? Slot : NextLink;
+
+    return (
+      <Comp
+        className={cn(
+          "rounded-sm outline-2 outline-offset-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:outline active:outline-purple-600",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
 
 Link.displayName = "Link";
 

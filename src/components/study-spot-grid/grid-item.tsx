e7fcbfcd3086813808a1c89b91f977b3924f2469
Charlie Zhao <charles.zhao5461@gmail.com>
2023-12-26 14:02:55 +1100

    Wrap grid-item in div for more flexibility

--- src/components/study-spot-grid/grid-item.tsx
+++ src/components/study-spot-grid/grid-item.tsx
@@ -6,12 +6,12 @@ import { type RouterOutputs } from "@/trpc/shared";
 
 import { Wifi, WifiOff, Zap, ZapOff, TreePine } from "lucide-react";
 
-import SkeletonGridItem from "./grid-item-skeleton";
-import ImageCarousel from "./image-carousel";
-import UnmountAfter from "../unmount-after";
-import { Skeleton } from "../ui/skeleton";
-import { Link } from "@/components/ui/link";
 import { isEmptyString } from "@/lib/utils";
+import { Link } from "@/components/ui/link";
+import SkeletonGridItem from "@/components/study-spot-grid/grid-item-skeleton";
+import ImageCarousel from "@/components/study-spot-grid/image-carousel";
+import UnmountAfter from "@/components/unmount-after";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const TooltipBase = dynamic(() => import("./tooltip-base"), {
   ssr: false,
@@ -45,79 +45,81 @@ export default function GridItem({
     "No Location";
 
   return (
-    <Link
-      key={id}
-      href={`/study-spot/${slug}`}
-      className="group relative h-fit rounded focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
-    >
-      {/* Add offset to prevent disgusting "flasing" umount */}
-      <UnmountAfter delay={i * 50 + 200}>
-        <SkeletonGridItem
-          className="duration-250 absolute -z-10 w-full animate-fade-out opacity-0"
+    <div className="relative">
+      <Link
+        key={id}
+        href={`/study-spot/${slug}`}
+        className="group relative block h-fit rounded focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+      >
+        {/* Add offset to prevent disgusting "flasing" umount */}
+        <UnmountAfter delay={i * 50 + 200}>
+          <SkeletonGridItem
+            className="duration-250 absolute -z-10 w-full animate-fade-out opacity-0"
+            style={{
+              animationDelay: `${i * 50}ms`,
+              animationFillMode: "backwards",
+            }}
+          />
+        </UnmountAfter>
+
+        <div
+          className="duration-250 animate-fade-in space-y-4"
           style={{
             animationDelay: `${i * 50}ms`,
             animationFillMode: "backwards",
           }}
-        />
-      </UnmountAfter>
-
-      <div
-        className="duration-250 animate-fade-in space-y-4"
-        style={{
-          animationDelay: `${i * 50}ms`,
-          animationFillMode: "backwards",
-        }}
-      >
-        <ImageCarousel
-          images={images}
-          name={name}
-          sizes="(max-width: 1024px) 40vw, 22vw"
-        />
-        <ul>
-          <li className="truncate text-ellipsis font-bold">{location}</li>
-          <li>
-            <h2 className="truncate text-ellipsis">{name}</h2>
-          </li>
-          <li className="truncate text-ellipsis">{venueType}</li>
-          <li className="mt-2">
-            <ul className="flex gap-2">
-              <li>
-                {wifi ? (
-                  <TooltipBase content={"Has Wifi"}>
-                    <Wifi />
-                  </TooltipBase>
-                ) : (
-                  <TooltipBase content={"Doesn't Have Wifi"}>
-                    <WifiOff className="text-neutral-200" />
-                  </TooltipBase>
-                )}
-              </li>
-              <li>
-                {powerOutlets ? (
-                  <TooltipBase content={"Has Power Outlets"}>
-                    <Zap />
-                  </TooltipBase>
-                ) : (
-                  <TooltipBase content={"Doesn't Have Power Outlets"}>
-                    <ZapOff className="text-neutral-200" />
-                  </TooltipBase>
-                )}
-              </li>
-              <li>
-                {naturalViews ? (
-                  <TooltipBase content={"Has Natural Views"}>
-                    <TreePine />
-                  </TooltipBase>
-                ) : (
-                  <TooltipBase content={"Doesn't Have Natural Views"}>
-                    <TreePine className="text-neutral-200" />
-                  </TooltipBase>
-                )}
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </Link>
+        >
+          <ImageCarousel
+            images={images}
+            name={name}
+            sizes="(max-width: 1024px) 40vw, 22vw"
+          />
+          <ul>
+            <li className="truncate text-ellipsis font-bold">{location}</li>
+            <li>
+              <h2 className="truncate text-ellipsis">{name}</h2>
+            </li>
+            <li className="truncate text-ellipsis">{venueType}</li>
+            <li className="mt-2">
+              <ul className="flex gap-2">
+                <li>
+                  {wifi ? (
+                    <TooltipBase content={"Has Wifi"}>
+                      <Wifi />
+                    </TooltipBase>
+                  ) : (
+                    <TooltipBase content={"Doesn't Have Wifi"}>
+                      <WifiOff className="text-neutral-200" />
+                    </TooltipBase>
+                  )}
+                </li>
+                <li>
+                  {powerOutlets ? (
+                    <TooltipBase content={"Has Power Outlets"}>
+                      <Zap />
+                    </TooltipBase>
+                  ) : (
+                    <TooltipBase content={"Doesn't Have Power Outlets"}>
+                      <ZapOff className="text-neutral-200" />
+                    </TooltipBase>
+                  )}
+                </li>
+                <li>
+                  {naturalViews ? (
+                    <TooltipBase content={"Has Natural Views"}>
+                      <TreePine />
+                    </TooltipBase>
+                  ) : (
+                    <TooltipBase content={"Doesn't Have Natural Views"}>
+                      <TreePine className="text-neutral-200" />
+                    </TooltipBase>
+                  )}
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </Link>
+    </div>
   );
 }

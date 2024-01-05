5ee2eb96daa087c9c7933e893cf5150fa0e2ae3d
Charlie Zhao <charles.zhao5461@gmail.com>
2024-01-05 16:03:24 +1100

    Fix edit account error message spacing

--- src/app/account/edit/layout.tsx
+++ src/app/account/edit/layout.tsx
@@ -13,7 +13,7 @@ export default async function Layout({
 
   return (
     <div className="mx-auto max-w-screen-2xl space-y-4 p-4">
-      <div className="rounded border p-4">
+      <div className="space-y-4 rounded border p-4">
         <h1 className="mb-4 text-lg font-bold underline">Edit User 👶</h1>
         <UserStatusWrapper>{children}</UserStatusWrapper>
       </div>

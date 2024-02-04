94f37696de0968e7d85b1a4165217af2ee380be9
Charlie Zhao <charles.zhao5461@gmail.com>
2024-02-04 17:46:37 +1100

    ssr edit spot form page, pre-populate form values by dehydrating

--- src/app/study-spot/[slug]/edit/layout.tsx
+++ src/app/study-spot/[slug]/edit/layout.tsx
@@ -1,21 +1,9 @@
-import { getServerAuthSession } from "@/server/auth";
-import UserStatusWrapper from "./user-status-wrapper";
-import { redirect } from "next/navigation";
-
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerAuthSession();
-
-  if (!session) redirect("/auth/signin");
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto max-w-screen-2xl space-y-4 p-4">
       <div className="space-y-4 rounded border p-4">
         <h1 className="mb-4 text-lg font-bold underline">Edit Spot 🔧</h1>
-        <UserStatusWrapper>{children}</UserStatusWrapper>
+        {children}
       </div>
     </div>
   );
--- src/app/study-spot/[slug]/edit/page.tsx
+++ src/app/study-spot/[slug]/edit/page.tsx
@@ -1,151 +1,44 @@
-"use client";
-
-import ServerErrorMessage from "@/components/server-error-message";
-import ServerZodError from "@/components/server-zod-error";
-import { uploadFilesToS3UsingPresignedUrls } from "@/lib/helpers";
-import type { CreateUpdateFormValues } from "@/schemas";
-import { api } from "@/trpc/react";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { redirect } from "next/navigation";
 
-const CreateUpdateSpotForm = dynamic(
-  () => import("@/components/create-update-spot-form"),
-  { ssr: false, loading: () => <div className="">Loading Form 📝...</div> },
+import { getServerAuthSession } from "@/server/auth";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import UserStatusWrapper from "./user-status-wrapper";
+import ReactQueryHydrate from "@/components/react-query-hydrate";
+import { createSSRHelper } from "@/server/api/ssr";
+import { dehydrate } from "@tanstack/react-query";
+
+const EditSpotFormController = dynamic(
+  () =>
+    import("@/components/create-update-spot-form/edit-spot-form-controller"),
+  {
+    ssr: false,
+    loading: () => <Skeleton className="h-96 w-full" />,
+  },
 );
 
-export default function EditStudySpotPage({
+export default async function EditSpotPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  // To capture the form data from the form component
-  const [formData, setFormData] = useState<CreateUpdateFormValues>();
+  const session = await getServerAuthSession();
 
-  const apiUtils = api.useUtils();
-  const router = useRouter();
+  if (!session) redirect("/auth/signin");
 
-  const { data, isLoading } = api.studySpot.bySlug.useQuery(params.slug);
-
-  const {
-    mutate: update,
-    isLoading: updateLoading,
-    error: updateError,
-    isSuccess: updateSuccess,
-  } = api.studySpot.update.useMutation({
-    onSuccess: (res) => {
-      void apiUtils.studySpot.bySlug.invalidate(res.slug);
-      router.push(`/study-spot/${res.slug}`);
-    },
-  });
-
-  const {
-    mutate: getPresignedUrl,
-    isLoading: presignedUrlsLoading,
-    error: presignedUrlsError,
-  } = api.studySpot.getPresignedUrls.useMutation({
-    onSuccess: async (presignedUrls) => {
-      if (!formData || !data) return;
-
-      // Handle existing images
-      const existingImages = formData.images.existingImages.filter(
-        (image, i) =>
-          image.delete || image.featured !== data.images[i]?.featured,
-      );
-      const existingImagesPayload =
-        existingImages.length > 0 ? existingImages : undefined;
-
-      // Early update if no new images
-      if (!presignedUrls) {
-        update({
-          ...formData,
-          id: data.id,
-          images: {
-            existingImages: existingImagesPayload,
-          },
-        });
-        return;
-      }
-
-      // Handle new images
-      const imageUrls = await uploadFilesToS3UsingPresignedUrls(
-        presignedUrls,
-        formData.images.newImages.map((image) => image.file),
-      );
-      const newImages = imageUrls.map((url, index) => ({
-        url,
-        featured: formData.images.newImages[index]?.featured ?? false,
-      }));
-      const newImagePayload = newImages.length > 0 ? newImages : undefined;
-
-      update({
-        ...formData,
-        id: data.id,
-        images: {
-          newImages: newImagePayload,
-          existingImages: existingImagesPayload,
-        },
-      });
-    },
-  });
-
-  function handleSubmit(formValues: CreateUpdateFormValues) {
-    setFormData(formValues);
-
-    const newImages = formValues.images.newImages.map((image) => ({
-      contentLength: image.file.size,
-      contentType: image.file.type,
-    }));
-
-    getPresignedUrl({
-      ...formValues,
-      images: newImages,
-      id: data?.id,
-    });
-  }
-
-  function getButtonText() {
-    const hasNewImages = !!formData?.images?.newImages?.length;
-
-    if (presignedUrlsLoading && hasNewImages) return "Uploading images...";
-    if (presignedUrlsLoading) return "Updating...";
-    if (updateLoading) return "Updating...";
-    if (updateSuccess) return "Redirecting you now...";
-
-    return "Submit";
-  }
-
-  const submitDisabled = updateLoading || presignedUrlsLoading || updateSuccess;
-
-  if (isLoading) {
-    return <div>Loading spot details 📍...</div>;
-  }
-
-  if (!data) {
-    return <div>Spot not found!</div>;
-  }
+  const helpers = await createSSRHelper();
+  await Promise.all([
+    helpers.user.currentBySession.prefetch(),
+    helpers.studySpot.bySlug.prefetch(params.slug),
+  ]);
+  const dehydratedState = dehydrate(helpers.queryClient);
 
   return (
-    <>
-      <CreateUpdateSpotForm
-        initialValues={data}
-        onSubmit={handleSubmit}
-        buttonLabel={getButtonText()}
-        submitDisabled={submitDisabled}
-      />
-
-      {!!presignedUrlsError?.data?.zodError && (
-        <ServerZodError zodError={presignedUrlsError.data.zodError} />
-      )}
-
-      {!!updateError?.data?.zodError && (
-        <ServerZodError zodError={updateError.data.zodError} />
-      )}
-
-      <ServerErrorMessage
-        message={presignedUrlsError?.message ?? updateError?.message}
-        code={presignedUrlsError?.data?.code ?? updateError?.data?.code}
-      />
-    </>
+    <ReactQueryHydrate state={dehydratedState}>
+      <UserStatusWrapper>
+        <EditSpotFormController slug={params.slug} />
+      </UserStatusWrapper>
+    </ReactQueryHydrate>
   );
 }

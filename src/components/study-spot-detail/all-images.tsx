c639646f25ad2e97a1f270628cb72a5966617205
Charlie Zhao <charles.zhao5461@gmail.com>
2024-02-11 20:22:47 +1100

    Style allimages for spotdetails

--- src/components/study-spot-detail/all-images.tsx
+++ src/components/study-spot-detail/all-images.tsx
@@ -7,18 +7,18 @@ type StudySpot = RouterOutputs["studySpot"]["bySlug"];
 
 export default function AllImages({ studySpot }: { studySpot?: StudySpot }) {
   return (
-    <div className="grid h-fit grid-cols-3 gap-2 md:grid-cols-8">
+    <div className="grid h-fit grid-cols-3 gap-2 md:grid-cols-5">
       {studySpot ? (
         studySpot?.images.slice(0, 15).map((image) => {
           return (
             <div
-              className="relative aspect-square"
+              className="relative aspect-square bg-neutral-100"
               key={`all-images-item-${image.id}`}
             >
               <ImageThatFadesIn
                 src={image.url}
                 alt={`Image of ${studySpot?.name}`}
-                className="object-cover"
+                className="object-contain p-4"
                 imageReady
                 fill
                 sizes="(max-width: 767px) 33vw, 12.5vw"

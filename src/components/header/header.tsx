297a5f4310fc19a92d841047246d0db6f1d2c967
Charlie Zhao <charles.zhao5461@gmail.com>
2024-02-25 11:41:27 +1100

    Remove global layout fetch as it delays TTFB on cold starts too much

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,16 @@
 import HeaderContent from "./header-content";
+import { createSSRHelper } from "@/server/api/ssr";
+import { dehydrate } from "@tanstack/react-query";
+import ReactQueryHydrate from "@/components/react-query-hydrate";
 
-export default function Header() {
-  return <HeaderContent />;
+export default async function Header() {
+  const helpers = await createSSRHelper();
+  await helpers.user.currentBySession.prefetch();
+  const dehydratedState = dehydrate(helpers.queryClient);
+
+  return (
+    <ReactQueryHydrate state={dehydratedState}>
+      <HeaderContent />
+    </ReactQueryHydrate>
+  );
 }
